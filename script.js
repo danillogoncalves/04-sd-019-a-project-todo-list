dd017720246f9f7333647e837e8959b92bcf738c
danillo <danillogonvalves001@gmail.com>
2021-12-10 16:52:10 -0300

    Sétimo requisito feito.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ buttonAdd.addEventListener('click', toDoList);
 function toDoList() {
     const orderedList = document.querySelector('#lista-tarefas');
     const task = document.createElement('li')
+    task.classList.add('task');
     const inputTask = document.querySelector('#texto-tarefa').value;
     const clearInput = document.querySelector('#texto-tarefa');
     if (inputTask === '') {
@@ -12,4 +13,14 @@ function toDoList() {
         orderedList.appendChild(task);
     }
     clearInput.value = '';
+    markOnClick()
+}
+
+function markOnClick() {
+    const toDoList = document.querySelectorAll('.task');
+    for (let i = 0; i < toDoList.length; i += 1) {
+        toDoList[i].addEventListener('click', function(e) {
+            e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        })
+    }
 }
