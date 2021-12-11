508a1b9a87c3dcf9eb70731ee6dbb286cc819d19
danillo <danillogonvalves001@gmail.com>
2021-12-11 10:59:33 -0300

    Melhorando o código seguindo as dicas da Luá Octaviano - Turma 19 - Tribo A.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function toDoList() {
     const task = document.createElement('li')
     task.classList.add('task');
     task.addEventListener('dblclick', markCompleted);
+    task.addEventListener('click', markOnClick);
     const inputTask = document.querySelector('#texto-tarefa').value;
     const clearInput = document.querySelector('#texto-tarefa');
     if (inputTask === '') {
@@ -14,22 +15,15 @@ function toDoList() {
         orderedList.appendChild(task);
     }
     clearInput.value = '';
-    markOnClick()
-    // markCompleted()
 }
 
-function markOnClick() {
-    const toDoList = document.querySelectorAll('.task');
-    for (let i = 0; i < toDoList.length; i += 1) {
-        toDoList[i].addEventListener('click', function(e) {
-            if (document.querySelector('.mark-task') !== null) {
-                document.querySelector('.mark-task').classList.remove('mark-task');
-            }
-            e.target.classList.add('mark-task');
-        })
+function markOnClick(e) {
+    if (document.querySelector('.mark-task') !== null) {
+        document.querySelector('.mark-task').classList.remove('mark-task');
     }
+    e.target.classList.add('mark-task');
 }
-
+// Quem me ensino a fazer a função dessa forma e como funciona o event.target foi a Luá Octaviano - Turma 19 - Tribo A
 function markCompleted(e) {
     console.log(e);
     e.target.classList.toggle('completed');
